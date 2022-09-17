4bc68acd10bdfcad1d0300d210a0f64c8229cc3c
dennisrotondi <dennisrotondi6@gmail.com>
2022-09-17 15:51:58 +0200

    download updated

--- datasets/download_dataset.sh
+++ datasets/download_dataset.sh
@@ -8,9 +8,9 @@ wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip
 # VQA Input Images
 wget http://images.cocodataset.org/zips/train2014.zip
 wget http://images.cocodataset.org/zips/val2014.zip
-wget http://images.cocodataset.org/zips/test2015.zip
+#wget http://images.cocodataset.org/zips/test2015.zip we do not download it to save space
 # Complementary Pairs List
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Train_mscoco.zip
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Val_mscoco.zip
 # Unzip
-unzip *.zip
+unzip \*.zip
